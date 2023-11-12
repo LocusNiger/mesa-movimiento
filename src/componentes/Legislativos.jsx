06157489b44fb1bca45c30d6cc614518dbdaeb88
Iván Sánchez <sanchezivan.798@gmail.com>
2023-11-12 20:30:33 -0300

    Fix capitalization in note titles and update note requirements

--- src/componentes/Legislativos.jsx
+++ src/componentes/Legislativos.jsx
@@ -8,11 +8,11 @@ const Legislativos = () => {
       </h1>
       <Accordion
         title="Nota Oficial"
-        content="Toda nota oficial debe ser presentada en hoja membretada, con sello oficial, firma original y aclaración."
+        content="Toda Nota Oficial debe ser presentada en hoja membretada, con sello oficial, firma original y aclaración."
       />
       <Accordion
         title="Nota Particular"
-        content="Toda nota particular debe cumplir con los siguientes requisitos que debemos verificar a la hora de recibirla: a quien va dirigida la nota (Presidente/a de la Cámara, cualquier Senador/ra cualquier área que forme parte de la institución), controlar si adjuntan o no documentación anexa, firma original con la aclaración, número de DNI y hojas enumeradas."
+        content="Toda Nota Particular debe cumplir con los siguientes requisitos que debemos verificar a la hora de recibirla: a quien va dirigida la nota (Presidente/a de la Cámara, cualquier Senador/ra cualquier área que forme parte de la institución), controlar si adjuntan o no documentación anexa, firma original con la aclaración, número de DNI y hojas enumeradas."
       />
     </section>
   );
